50d605fa0c80aac740ee43ea6ae54e8e03237358
Bojan Čoka <metropolis.lights@gmail.com>
2015-05-14 15:11:42 +0200

    Add a status bar.

--- src/main/java/gui/BottomPanel.java
+++ src/main/java/gui/BottomPanel.java
@@ -4,9 +4,17 @@ import javax.swing.*;
 
 public class BottomPanel extends JPanel
 {
+  private JPanel[] panelArray = new JPanel[5];
+
   public BottomPanel()
   {
-    super();
-    this.add(new JLabel("PLACEHOLDER"));
+    // use the implied FlowLayout
+    for (int i = 0; i < panelArray.length; i++)
+    {
+      JPanel panel = new JPanel();
+      if (i == 0) { panel.add(new JLabel("Position:")); }
+      else { panel.add(new JLabel("Graph editor")); }
+      this.add(panel);
+    }
   }
 }
