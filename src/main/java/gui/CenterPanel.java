845e144047db6adbbdee2240506a580681c840c2
Bojan Čoka <metropolis.lights@gmail.com>
2015-05-17 22:43:22 +0200

    Add a diagram view.

--- src/main/java/gui/CenterPanel.java
+++ src/main/java/gui/CenterPanel.java
@@ -2,11 +2,26 @@ package gui;
 
 import javax.swing.*;
 
-public class CenterPanel extends JPanel
+public class CenterPanel extends JDesktopPane
 {
+  private static int diagramCount = 0;
+  private int xOffset = 24, yOffset = 24;
+
   public CenterPanel()
   {
     super();
-    this.add(new JLabel("PLACEHOLDER"));
+    for (int i = 0; i < 5; i++)
+    {
+      Diagram diagram = this.create_diagram();
+      this.add(diagram);
+      diagram.moveToFront();
+    }
+  }
+
+  private Diagram create_diagram()
+  {
+    Diagram diagram = new Diagram(diagramCount * xOffset, diagramCount * yOffset);
+    this.diagramCount++;
+    return diagram;
   }
 }
---
+++ src/main/java/gui/Diagram.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JInternalFrame;
+
+public class Diagram extends JInternalFrame
+{
+  public Diagram(int x, int y)
+  {
+    super("PLACEHOLDER", true, true, true, true);
+    this.setLocation(x, y);
+    this.setSize(400, 400);
+    this.setVisible(true);
+  }
+}
