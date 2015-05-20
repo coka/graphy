ea30d84f8bbd41f560bb580cfda1a24fcfb6c06f
Bojan Čoka <metropolis.lights@gmail.com>
2015-05-21 01:07:30 +0200

    Populate the left panel with a default JTree.

--- src/main/java/gui/LeftPanel.java
+++ src/main/java/gui/LeftPanel.java
@@ -1,12 +1,13 @@
 package gui;
 
+import java.awt.*;
 import javax.swing.*;
 
-public class LeftPanel extends JPanel
+public class LeftPanel extends JTree
 {
   public LeftPanel()
   {
     super();
-    this.add(new JLabel("PLACEHOLDER"));
+    this.setPreferredSize(new Dimension(200, 600));
   }
 }
