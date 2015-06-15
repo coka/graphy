3b91c710512d5929a97e0a8b6ad76efd32a1bd22
Sinisa Pejin <sinisapejin@gmail.com>
2015-06-15 15:03:02 +0200

    Triangle and Star shapes

---
+++ src/main/java/shapes/TriangleShape.java
@@ -0,0 +1,51 @@
+package shapes;
+
+import java.awt.*;
+import java.awt.geom.*;
+
+import helpers.*;
+
+public class TriangleShape extends AbstractShape
+{
+  private float width, height;
+
+  public TriangleShape(Vec2f position, float size)
+  {
+    super(position);
+    this.width  = size;
+    this.height = size;
+    this.construct_shape();
+  }
+
+  public TriangleShape(Vec2f position, float width, float height)
+  {
+    super(position);
+    this.width  = width ;
+    this.height = height;
+    this.construct_shape();
+  }
+
+  public TriangleShape(Vec2f position, Color stroke, Color fill, float size)
+  {
+    super(position, stroke, fill);
+    this.width  = size;
+    this.height = size;
+    this.construct_shape();
+  }
+
+  public TriangleShape(Vec2f position, Color stroke, Color fill, float width, float height)
+  {
+    super(position, stroke, fill);
+    this.width  = width ;
+    this.height = height;
+    this.construct_shape();
+  }
+
+  public void construct_shape()
+  {
+    ((GeneralPath)this.get_shape()).moveTo(this.get_position().x + this.width/2 , this.get_position().y              );    
+    ((GeneralPath)this.get_shape()).lineTo(this.get_position().x + this.width, this.get_position().y + this.height);
+    ((GeneralPath)this.get_shape()).lineTo(this.get_position().x             , this.get_position().y + this.height);
+    ((GeneralPath)this.get_shape()).closePath();
+  }
+}
--- src/main/java/views/GraphicsView.java
+++ src/main/java/views/GraphicsView.java
@@ -16,5 +16,11 @@ public class GraphicsView extends JComponent
 
     RectangleShape shape = new RectangleShape(new Vec2f(), Color.RED, Color.GREEN, 100.0f);
     shape.draw(g2);
+    
+    TriangleShape triangle = new TriangleShape(new Vec2f(), Color.BLACK, Color.WHITE, 90.0f);
+    triangle.draw(g2);
+    
+    StarShape star = new StarShape(new Vec2f(), Color.YELLOW, Color.BLACK, 80.0f);
+    star.draw(g2);
   }
 }
